db2111d31cd1cb7a2d65b662276dd0b98124d292
Jhansi <madugulajhansi6@gmail.com>
2025-02-24 07:00:36 +0530

    Resolved merge conflicts

--- app.py
+++ app.py
@@ -335,6 +335,7 @@ def search_restaurants_by_image():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's dynamic PORT
+    app.run(host="0.0.0.0", port=port, debug=True)
 
 
